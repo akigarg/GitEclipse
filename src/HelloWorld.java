cd8102278c8d2806ab474c725f20e4042ab41ea9
Ankit Garg <ankit.niitian@gmail.com>
2018-04-04 23:15:50 +0530

    Revert "more changes"
    This reverts commit c0cbf032c74487a2bc654724d5dabe7c1e047419.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello New Git World!!!");
 		System.out.println("Some Crazy Local Change...");
-		System.out.println("Even Further Changes");
 	}
 }
