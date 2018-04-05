70e224265ee606d1d75ccf6b2be05cc8ab6e1846
Ankit Garg <ankit.niitian@gmail.com>
2018-04-05 23:50:23 +0530

    Fresh start from Console

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,9 +1,6 @@
 
 public class HelloWorld2 {
 	public static void main(String[] args) {
-		System.out.println("Eureka....");
-		System.out.println("Edited outside Eclipse.");
-		System.out.println("Edited outside Eclipse again.");
-		System.out.println("Edited outside Eclipse one more time.");
+		System.out.println("Eureka Again....");
 	}
 }
